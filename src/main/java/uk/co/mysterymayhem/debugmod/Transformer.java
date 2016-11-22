18085d2a8c53ecd78cc4c930f66bc00bae2fb9de
Thomas Barlow <github@mysterymayhem.co.uk>
2016-11-22 10:04:08 +0000

    Forgot Transformer class

--- src/main/java/uk/co/mysterymayhem/debugmod/Transformer.java
+++ src/main/java/uk/co/mysterymayhem/debugmod/Transformer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uk.co.mysterymayhem.tessellatorfix;
+package uk.co.mysterymayhem.debugmod;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -15,23 +15,28 @@ import java.util.function.Function;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
 import net.minecraftforge.fml.common.FMLLog;
+import org.objectweb.asm.util.Textifier;
 import org.objectweb.asm.util.TraceClassVisitor;
+import org.objectweb.asm.util.TraceMethodVisitor;
 
 /**
  * @author Mysteryem
  */
 public class Transformer implements IClassTransformer {
 
-    private static final String CLASS_NAME = "net.minecraft.client.renderer.Tessellator";
+//    private static final String CLASS_NAME = "net.minecraft.client.renderer.Tessellator";
 
     private static final HashMap<String, Function<byte[], byte[]>> classToMethodMap = new HashMap<>();
 
     static {
-        classToMethodMap.put("net.minecraft.entity.Entity$1", Transformer::patchEntity$1);
-        classToMethodMap.put("net.minecraft.entity.Entity", Transformer::patchEntity);
+//        classToMethodMap.put("net.minecraft.entity.Entity$1", Transformer::patchEntity$1);
+//        classToMethodMap.put("net.minecraft.entity.Entity", Transformer::patchEntity);
+        classToMethodMap.put("net.minecraft.entity.item.EntityBoat", Transformer::logEntityBoatGetDamageTaken);
+        classToMethodMap.put("net.minecraft.network.datasync.EntityDataManager", Transformer::patchEntityDataManager);
     }
 
 //  private int rawBufferIndex  = 0;
@@ -57,59 +62,71 @@ public class Transformer implements IClassTransformer {
         FMLLog.info(stringWriter.toString());
     }
 
-    private static byte[] patchEntity$1(byte[] bytes) {
-        FMLLog.info("[MystDebugMod] Printing Entity$1 to console/log:");
-        printClassToFMLLogger(bytes);
-        FMLLog.info("[MystDebugMod] Printed Entity$1 to constole/log");
-        return bytes;
+    private static void printMethodToFMLLogger(MethodNode methodNode) {
+        StringWriter stringWriter = new StringWriter();
+        stringWriter.append("\n");
+        PrintWriter writer = new PrintWriter(stringWriter);
+        Textifier textifier = new Textifier();
+        TraceMethodVisitor traceMethodVisitor = new TraceMethodVisitor(textifier);
+        methodNode.accept(traceMethodVisitor);
+        textifier.print(writer);
+
+        FMLLog.info(stringWriter.toString());
     }
 
-    private static byte[] patchEntity(byte[] bytes) {
-        FMLLog.info("[MystDebugMod] Patching Entity class");
-        final String readFromNBT_methodName = Plugin.runtimeDeobfEnabled ? "func_70020_e" : "readFromNBT";
-        final String makeCrashReport_name = Plugin.runtimeDeobfEnabled ? "func_85055_a" : "makeCrashReport";
+//    private static byte[] patchEntity$1(byte[] bytes) {
+//        FMLLog.info("[MystDebugMod] Printing Entity$1 to console/log:");
+//        printClassToFMLLogger(bytes);
+//        FMLLog.info("[MystDebugMod] Printed Entity$1 to constole/log");
+//        return bytes;
+//    }
+//
+//    private static byte[] patchEntity(byte[] bytes) {
+//        FMLLog.info("[MystDebugMod] Patching Entity class");
+//        final String readFromNBT_methodName = Plugin.runtimeDeobfEnabled ? "func_70020_e" : "readFromNBT";
+//        final String makeCrashReport_name = Plugin.runtimeDeobfEnabled ? "func_85055_a" : "makeCrashReport";
+//
+//        ClassNode classNode = new ClassNode();
+//        ClassReader classReader = new ClassReader(bytes);
+//        classReader.accept(classNode, 0);
+//
+//        boolean patchSuccessful = false;
+//
+//        outerfor:
+//        for (MethodNode methodNode : classNode.methods) {
+//            if (methodNode.name.equals(readFromNBT_methodName)) {
+//                for (ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator(); iterator.hasNext(); ) {
+//                    AbstractInsnNode next = iterator.next();
+//                    if (next instanceof MethodInsnNode) {
+//                        MethodInsnNode methodInsnNode = (MethodInsnNode)next;
+//                        if (methodInsnNode.name.equals(makeCrashReport_name)) {
+//                            iterator.add(new VarInsnNode(Opcodes.ALOAD, 0)); //this
+//                            iterator.add(new VarInsnNode(Opcodes.ALOAD, 2)); //this, throwable
+//                            iterator.add(new MethodInsnNode(
+//                                    Opcodes.INVOKESTATIC,
+//                                    "uk/co/mysterymayhem/debugmod/Hooks",
+//                                    "debugHook",
+//                                    "(Lnet/minecraft/entity/Entity;Ljava/lang/Throwable;)V", false));
+//                            patchSuccessful = true;
+//                            break outerfor;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        if (!patchSuccessful) {
+//            throw new RuntimeException("Failed to patch Entity class");
+//        }
+//
+//        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+//        classNode.accept(classWriter);
+//
+//        FMLLog.info("[MystDebugMod] Patched Entity class");
+//        return classWriter.toByteArray();
+//    }
 
-        ClassNode classNode = new ClassNode();
-        ClassReader classReader = new ClassReader(bytes);
-        classReader.accept(classNode, 0);
-
-        boolean patchSuccessful = false;
-
-        outerfor:
-        for (MethodNode methodNode : classNode.methods) {
-            if (methodNode.name.equals(readFromNBT_methodName)) {
-                for (ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator(); iterator.hasNext(); ) {
-                    AbstractInsnNode next = iterator.next();
-                    if (next instanceof MethodInsnNode) {
-                        MethodInsnNode methodInsnNode = (MethodInsnNode)next;
-                        if (methodInsnNode.name.equals(makeCrashReport_name)) {
-                            iterator.add(new VarInsnNode(Opcodes.ALOAD, 0)); //this
-                            iterator.add(new VarInsnNode(Opcodes.ALOAD, 2)); //this, throwable
-                            iterator.add(new MethodInsnNode(
-                                    Opcodes.INVOKESTATIC,
-                                    "uk/co/mysterymayhem/tessellatorfix/Hooks",
-                                    "debugHook",
-                                    "(Lnet/minecraft/entity/Entity;Ljava/lang/Throwable;)V", false));
-                            patchSuccessful = true;
-                            break outerfor;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!patchSuccessful) {
-            throw new RuntimeException("Failed to patch Entity class");
-        }
-
-        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        classNode.accept(classWriter);
-
-        FMLLog.info("[MystDebugMod] Patched Entity class");
-        return classWriter.toByteArray();
-    }
-
-    public static byte[] patchEntityDataManager(byte[] bytes) {
+    private static byte[] patchEntityDataManager(byte[] bytes) {
         String targetMethodName = Plugin.runtimeDeobfEnabled ? "func_187227_b" : "set";
 
         FMLLog.info("[MystDebugMod] Patching EntityDataManager class");
@@ -120,6 +137,7 @@ public class Transformer implements IClassTransformer {
 
         boolean patchSuccessful = false;
 
+        outerfor:
         for (MethodNode methodNode : classNode.methods) {
             if (methodNode.name.equals(targetMethodName)) {
                 for (ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator(); iterator.hasNext(); ) {
@@ -129,14 +147,21 @@ public class Transformer implements IClassTransformer {
                         iterator.previous();
                         iterator.add(new VarInsnNode(Opcodes.ALOAD, 1)); // DataParameter<T> key
                         iterator.add(new VarInsnNode(Opcodes.ALOAD, 2)); // key, T value
-                        iterator.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "owner", "name", "desc", false));
+                        iterator.add(new MethodInsnNode(
+                                Opcodes.INVOKESTATIC,
+                                "uk/co/mysterymayhem/debugmod/Hooks",
+                                "dataManagerDebugHook",
+                                "(Lnet/minecraft/network/datasync/DataParameter;Ljava/lang/Object;)V", false));
+                        patchSuccessful = true;
+                        break outerfor;
                     }
                 }
             }
         }
 
-
-
+        if (!patchSuccessful) {
+            throw new RuntimeException("[MystDebugMod] EntityDataManager patch unsuccessful");
+        }
 
         ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
         classNode.accept(classWriter);
@@ -145,66 +170,86 @@ public class Transformer implements IClassTransformer {
         return classWriter.toByteArray();
     }
 
-    private static byte[] patchTessellatorClass(byte[] bytes) {
-        String targetMethodName;
+    private static byte[] logEntityBoatGetDamageTaken(byte[] bytes) {
+        String targetMethodName = Plugin.runtimeDeobfEnabled ? "func_70271_g" : "getDamageTaken";
 
-        if (Plugin.runtimeDeobfEnabled) {
-            targetMethodName = "func_147564_a";
-        }
-        else {
-            targetMethodName = "getVertexState";
-        }
+        FMLLog.info("[MystDebugMod] Logging EntityBoat::getDamageTaken");
 
         ClassNode classNode = new ClassNode();
         ClassReader classReader = new ClassReader(bytes);
         classReader.accept(classNode, 0);
 
-        Iterator<MethodNode> methods = classNode.methods.iterator();
-        while (methods.hasNext()) {
-            MethodNode m = methods.next();
-            if ((m.name.equals(targetMethodName) && m.desc.equals("(FFF)Lnet/minecraft/client/shader/TesselatorVertexState;"))) {
-                FMLLog.info("Inside target Tessellator method");
-
-                InsnList toInject = new InsnList();
-
-                // Insertion of "if (this.rawBufferIndex < 1) return"
-                LabelNode labelNode = new LabelNode();
-
-                toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                String fieldName;
-                if (Plugin.runtimeDeobfEnabled) {
-                    fieldName = "field_147569_p";
-                }
-                else {
-                    fieldName = "rawBufferIndex";
-                }
-                toInject.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/renderer/Tessellator", fieldName, "I"));
-                toInject.add(new InsnNode(Opcodes.ICONST_1));
-                toInject.add(new JumpInsnNode(Opcodes.IF_ICMPGE, labelNode));
-                toInject.add(new InsnNode(Opcodes.ACONST_NULL));
-                toInject.add(new InsnNode(Opcodes.ARETURN));
-                toInject.add(labelNode);
-
-
-                // Insert after
-                m.instructions.insert(toInject);
-
-                ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
-                classNode.accept(writer);
-                FMLLog.info("Exiting target Tessellator method");
-                return writer.toByteArray();
+        for (MethodNode methodNode : classNode.methods) {
+            if (methodNode.name.equals(targetMethodName)) {
+                printMethodToFMLLogger(methodNode);
+                FMLLog.info("[MystDebugMod] Logged EntityBoat::getDamageTaken");
+                return bytes;
             }
         }
 
-        FMLLog.warning("Could not find Tessellator method out of:");
-        StringBuilder builder = new StringBuilder();
-        for (MethodNode methodNode : classNode.methods) {
-            builder.append(methodNode.name).append(":").append(methodNode.desc).append("\n");
-        }
-        FMLLog.info(builder.toString());
-
         return bytes;
     }
+
+//    private static byte[] patchTessellatorClass(byte[] bytes) {
+//        String targetMethodName;
+//
+//        if (Plugin.runtimeDeobfEnabled) {
+//            targetMethodName = "func_147564_a";
+//        }
+//        else {
+//            targetMethodName = "getVertexState";
+//        }
+//
+//        ClassNode classNode = new ClassNode();
+//        ClassReader classReader = new ClassReader(bytes);
+//        classReader.accept(classNode, 0);
+//
+//        Iterator<MethodNode> methods = classNode.methods.iterator();
+//        while (methods.hasNext()) {
+//            MethodNode m = methods.next();
+//            if ((m.name.equals(targetMethodName) && m.desc.equals("(FFF)Lnet/minecraft/client/shader/TesselatorVertexState;"))) {
+//                FMLLog.info("Inside target Tessellator method");
+//
+//                InsnList toInject = new InsnList();
+//
+//                // Insertion of "if (this.rawBufferIndex < 1) return"
+//                LabelNode labelNode = new LabelNode();
+//
+//                toInject.add(new VarInsnNode(Opcodes.ALOAD, 0));
+//                String fieldName;
+//                if (Plugin.runtimeDeobfEnabled) {
+//                    fieldName = "field_147569_p";
+//                }
+//                else {
+//                    fieldName = "rawBufferIndex";
+//                }
+//                toInject.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/renderer/Tessellator", fieldName, "I"));
+//                toInject.add(new InsnNode(Opcodes.ICONST_1));
+//                toInject.add(new JumpInsnNode(Opcodes.IF_ICMPGE, labelNode));
+//                toInject.add(new InsnNode(Opcodes.ACONST_NULL));
+//                toInject.add(new InsnNode(Opcodes.ARETURN));
+//                toInject.add(labelNode);
+//
+//
+//                // Insert after
+//                m.instructions.insert(toInject);
+//
+//                ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES);
+//                classNode.accept(writer);
+//                FMLLog.info("Exiting target Tessellator method");
+//                return writer.toByteArray();
+//            }
+//        }
+//
+//        FMLLog.warning("Could not find Tessellator method out of:");
+//        StringBuilder builder = new StringBuilder();
+//        for (MethodNode methodNode : classNode.methods) {
+//            builder.append(methodNode.name).append(":").append(methodNode.desc).append("\n");
+//        }
+//        FMLLog.info(builder.toString());
+//
+//        return bytes;
+//    }
 
     @Override
     public byte[] transform(String className, String transformedClassName, byte[] bytes) {
